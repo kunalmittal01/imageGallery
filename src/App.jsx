917ca486cbe4059069327fef77034411ee00b1db
kunalmittal01 <kunalmittalmrt@gmail.com>
2024-10-13 18:52:54 +0530

    Image Gallery Added.

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import { useState,useEffect,useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App(e) {
+  const [menu, setMenu] = useState(false);
+  const [imageArr, setImageArr] = useState([]);
+  const [dispArr, setDispArr] = useState([]);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const inRef = useRef(null);
+  const showmenu = (e)=>{
+    if(menu) {
+      console.log(e.target,e.target.tagName);
+      if(e.target.tagName == 'I') {
+        e.target.parentElement.parentElement.style.height = '3.5rem';
+      }
+      else {
+        e.target.parentElement.style.height = '3.5rem';
+      }
+      setMenu(false);
+    }
+    else {
+      console.log(e.target);
+      if(e.target.tagName == 'DIV') {
+        e.target.parentElement.style.height = '5.8rem';
+      }
+      else {
+        e.target.parentElement.parentElement.style.height = '5.8rem';
+      }
+      setMenu(true);
+    }
+  }
+  const getImages = async(page) => {
+    let resp = await fetch(`https://api.unsplash.com/photos/?page=${page}&per_page=20&client_id=mKF2f2UHbRFgwLp9jIJ6knMi2a3RKMsVVnWHYOqFves`);
+    let data = await resp.json();
+    console.log(data);
+    
+    return data;
+  }
+  const setDisplayData = async(page)=>{
+    let data = await getImages(page);
+    let images = data.map(obj=>{
+      return {
+        id: obj.id,
+        url: obj.urls.small,
+        desc: obj.sponsorship?.tagline,
+        name: obj.user.name,
+        time: obj.updated_at
+      }
+    });
+    setImageArr(images);
+    setDispArr([...dispArr,...images]);
+  }
+
+  useEffect(()=>{
+    const handleScroll = async()=>{
+      if(window.scrollY + window.innerHeight + 30 >= document.body.offsetHeight) {
+        setDisplayData(page);
+        setPage((page)=>page+1);
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+    return ()=>{
+      window.removeEventListener('scroll', handleScroll);
+    }
+  },[window.scrollY]);
+
+  useEffect(()=>{
+    setDisplayData(1);
+    setPage(page+1);
+  },[])
+  return (
+    <>
+      <nav>
+        <p>ImageGallery</p>
+        <div className="search-cont">
+          <input ref={inRef} onKeyUp={(e)=>{if(e.key=="Enter")setQuery(e.target.value)}} type="text" placeholder="Search Images..."/>
+          <button onClick={()=>setQuery(inRef.current.value)}>Search</button>
+        </div>
+        <div onClick={showmenu} className="menu">
+          <i className="fa-solid fa-bars"></i>
+        </div>
+      </nav>
+      <div className="disp">
+        {
+          dispArr.filter((data)=>{
+            return data.desc?.toLowerCase().includes(query.toLowerCase()) || data.name.toLowerCase().includes(query.toLowerCase())
+          }).map(data => {
+            return (
+              <div key={data.id + Date.now()} className="card">
+                <img src={data.url} alt="" />
+                <div className="card-desc">
+                  <h2>{data.name}</h2>
+                  <p>{data.desc}</p>
+                  <p>{data.time}</p>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
